b6f34bc366ef55f4fc7d0f099147b59e846971c3
KhaderKaraja <khderjber@gmail.com>
2022-07-23 17:25:17 +0300

    All Done!

--- mac-changer.py
+++ mac-changer.py
@@ -2,6 +2,7 @@
 
 import subprocess
 import optparse
+import re
 
 def parse_input():
     parser = optparse.OptionParser()
@@ -15,12 +16,34 @@ def parse_input():
     return opts 
 
 def change_mac(interface, new_mac):
-    print("[+] Changing the address for interface " + interface + " to MAC address of " + new_mac)
+    print("[+] Changing the MAC address for interface " + interface + " to MAC address of " + new_mac)
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
 
+def get_interface_mac(interface):
+    ifconfig_interface_result = subprocess.check_output(["ifconfig", interface])
+    # \w\w:\w\w:\w\w:\w\w:\w\w:\w\w
+    search_match = re.search(r"(\w\w:){5}(\w\w){1}", str(ifconfig_interface_result))
+    if not search_match:
+        return None 
+    else:
+        return search_match.group(0)
+
+
 
 options = parse_input()
 
-change_mac(options.interface, options.new_mac)
+current_mac = get_interface_mac(options.interface)
+if not current_mac:
+    print("[-] This interface does not have a MAC address")
+elif current_mac == options.new_mac:
+    print("[-] This MAC is already assigned to this interface.")    
+else:
+    print("[+] The current MAC address is: " + current_mac)
+    change_mac(options.interface, options.new_mac)
+    current_mac = get_interface_mac(options.interface)
+    if current_mac == options.new_mac:
+        print("[+] The MAC address for the interface " + options.interface + " changed sucessfully to " + options.new_mac)
+    else:
+        print("[-] The MAC changing faild => try to run the script as an administrator using `sudo` prefix before running python commang")
